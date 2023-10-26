d7f3c21008bd6f287340c100001bea75db6190d0
Alkosz <147086689+Alkosz@users.noreply.github.com>
2023-10-26 17:07:51 +0200

    Update bot_discord.py

--- bot_discord.py
+++ bot_discord.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 from bot_logic import gen_pass
 
@@ -11,7 +12,7 @@ async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
 
 @bot.command()
-async def hello(ctx):
+async def cześć(ctx):
     await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
 @bot.command()
@@ -23,8 +24,19 @@ async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
+async def jaktam(ctx):
+    await ctx.send(f'Dziękuję, że pytasz, ale jestem tylko programem komputerowym, więc nie mam uczuć ani emocji.')
+
+@bot.command()
+async def kostka(ctx):
+    a = random.randint(1,6)
+    await ctx.send(f'Wylosowałeś ' + str(a))
+
+
+
+@bot.command()
 async def pa(ctx):
-    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+    await ctx.send(f'Papa')
 
 
 bot.run("MTE2MjA1NTg0MjYwMjk1NDc4Mg.G2mpFF.6Y65jgG4juFZfNt1MFtLqKq2bn28R2Dls2t-e4")
